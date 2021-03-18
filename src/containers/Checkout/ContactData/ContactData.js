0943cb1743dfe8872493ae3c97fe3a791bd8f37b
Manish Shah <manish55600>
2021-03-18 16:47:38 +0530

    Added email-password authentication to app, added lazy loading for performance optimization, added multiple reducers, actions and combined them in index file, added routing and guards to the routing based on authentication of user

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -7,158 +7,139 @@ import Input from "../../../components/UI/Input/Input";
 import { connect } from "react-redux";
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler'
 import * as orderActions from '../../../store/actions/index'
+import { updateObject, checkValidity } from '../../../shared/utility'
 
 class ContactData extends Component {
     state = {
         orderForm: {
             name: {
-                elementType: "input",
+                elementType: 'input',
                 elementConfig: {
-                    type: "text",
-                    placeholder: "Your Name",
+                    type: 'text',
+                    placeholder: 'Your Name'
                 },
-                value: "",
+                value: '',
                 validation: {
-                    required: true,
+                    required: true
                 },
                 valid: false,
-                touched: false,
+                touched: false
             },
             street: {
-                elementType: "input",
+                elementType: 'input',
                 elementConfig: {
-                    type: "text",
-                    placeholder: "Street",
+                    type: 'text',
+                    placeholder: 'Street'
                 },
-                value: "",
+                value: '',
                 validation: {
-                    required: true,
+                    required: true
                 },
                 valid: false,
-                touched: false,
+                touched: false
             },
             zipCode: {
-                elementType: "input",
+                elementType: 'input',
                 elementConfig: {
-                    type: "text",
-                    placeholder: "ZIP Code",
+                    type: 'text',
+                    placeholder: 'ZIP Code'
                 },
-                value: "",
+                value: '',
                 validation: {
                     required: true,
-                    minLength: 6,
-                    maxLength: 6,
+                    minLength: 5,
+                    maxLength: 5,
+                    isNumeric: true
                 },
                 valid: false,
-                touched: false,
+                touched: false
             },
             country: {
-                elementType: "input",
+                elementType: 'input',
                 elementConfig: {
-                    type: "text",
-                    placeholder: "Country",
+                    type: 'text',
+                    placeholder: 'Country'
                 },
-                value: "",
+                value: '',
                 validation: {
-                    required: true,
+                    required: true
                 },
                 valid: false,
-                touched: false,
+                touched: false
             },
             email: {
-                elementType: "input",
+                elementType: 'input',
                 elementConfig: {
-                    type: "email",
-                    placeholder: "Your E-Mail",
+                    type: 'email',
+                    placeholder: 'Your E-Mail'
                 },
-                value: "",
+                value: '',
                 validation: {
                     required: true,
+                    isEmail: true
                 },
                 valid: false,
-                touched: false,
+                touched: false
             },
             deliveryMethod: {
-                elementType: "select",
+                elementType: 'select',
                 elementConfig: {
                     options: [
-                        { value: "fastest", displayValue: "Fastest" },
-                        { value: "cheapest", displayValue: "Cheapest" },
-                    ],
+                        {value: 'fastest', displayValue: 'Fastest'},
+                        {value: 'cheapest', displayValue: 'Cheapest'}
+                    ]
                 },
-                value: "fastest",
+                value: 'fastest',
                 validation: {},
-                valid: true,
-            },
+                valid: true
+            }
         },
-        formIsValid: false,
-    };
+        formIsValid: false
+    }
 
-    orderHandler = (event) => {
+    orderHandler = ( event ) => {
         event.preventDefault();
         const formData = {};
         for (let formElementIdentifier in this.state.orderForm) {
-            formData[formElementIdentifier] = this.state.orderForm[
-                formElementIdentifier
-            ].value;
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
         }
         const order = {
             ingredients: this.props.ings,
             price: this.props.price,
             orderData: formData,
-        };
-        this.props.onOrderBurger(order)
-    };
-
-    checkValidity = (value, rules) => {
-        let isValid = true;
-        if (rules.required) {
-            isValid = value.trim !== "";
+            userId: this.props.userId
         }
-        if (rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid;
-        }
-        if (rules.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid;
-        }
-        return isValid;
-    };
+        this.props.onOrderBurger(order, this.props.token);
+    }
 
     inputChangedHandler = (event, inputIdentifier) => {
-        const updatedOrderForm = {
-            ...this.state.orderForm,
-        };
-        const updatedFormElement = {
-            ...updatedOrderForm[inputIdentifier],
-        };
-        updatedFormElement.value = event.target.value;
-        updatedFormElement.valid = this.checkValidity(
-            updatedFormElement.value,
-            updatedFormElement.validation
-        );
-        updatedFormElement.touched = true;
-        console.log(updatedFormElement);
-        updatedOrderForm[inputIdentifier] = updatedFormElement;
-
+        const updatedFormElement = updateObject(this.state.orderForm[inputIdentifier], {
+            value : event.target.value,
+            valid : checkValidity(event.target.value, this.state.orderForm[inputIdentifier].validation),
+            touched: true
+        })
+        const updatedOrderForm = updateObject(this.state.orderForm, {
+            [inputIdentifier] : updatedFormElement
+        })
         let formIsValid = true;
         for (let inputIdentifier in updatedOrderForm) {
             formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
         }
         this.setState({ orderForm: updatedOrderForm, formIsValid: formIsValid });
-    };
+    }
 
-    render() {
+    render () {
         const formElementsArray = [];
         for (let key in this.state.orderForm) {
             formElementsArray.push({
                 id: key,
-                config: this.state.orderForm[key],
+                config: this.state.orderForm[key]
             });
         }
         let form = (
             <form onSubmit={this.orderHandler}>
-                {formElementsArray.map((formElement) => (
-                    <Input
+                {formElementsArray.map(formElement => (
+                    <Input 
                         key={formElement.id}
                         elementType={formElement.config.elementType}
                         elementConfig={formElement.config.elementConfig}
@@ -166,15 +147,12 @@ class ContactData extends Component {
                         invalid={!formElement.config.valid}
                         shouldValidate={formElement.config.validation}
                         touched={formElement.config.touched}
-                        changed={(event) => this.inputChangedHandler(event, formElement.id)}
-                    />
+                        changed={(event) => this.inputChangedHandler(event, formElement.id)} />
                 ))}
-                <Button btnType="Success" disabled={!this.state.formIsValid}>
-                    ORDER
-        </Button>
+                <Button btnType="Success" disabled={!this.state.formIsValid}>ORDER</Button>
             </form>
         );
-        if (this.props.loading) {
+        if ( this.props.loading ) {
             form = <Spinner />;
         }
         return (
@@ -186,18 +164,20 @@ class ContactData extends Component {
     }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
     return {
         ings: state.burgerBuilder.ingredients,
         price: state.burgerBuilder.totalPrice,
-        loading: state.order.loading
-    };
+        loading: state.order.loading,
+        token: state.auth.token,
+        userId: state.auth.userId
+    }
 };
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
     return {
-        onOrderBurger: (orderData) => dispatch(orderActions.purchaseBurger(orderData))
-    }
-}
+        onOrderBurger: (orderData, token) => dispatch(orderActions.purchaseBurger(orderData, token))
+    };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(ContactData, axios));
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -3,34 +3,44 @@ import Aux from "../Auxiliary/Auxiliary";
 import classes from "./Layout.module.css";
 import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
 import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
+import { connect } from 'react-redux'
 
 class Layout extends Component {
-  state = {
-    showSideDrawer: false,
-  };
+    state = {
+        showSideDrawer: false,
+    };
 
-  sideDrawerClosedHandler = () => {
-    this.setState({ showSideDrawer: false });
-  };
+    sideDrawerClosedHandler = () => {
+        this.setState({ showSideDrawer: false });
+    };
 
-  sideDrawerToggleHandler = () => {
-    this.setState((prevState) => {
-      return { showSideDrawer: !prevState.showSideDrawer };
-    });
-  };
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        });
+    };
 
-  render() {
-    return (
-      <Aux>
-        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
-        <SideDrawer
-          open={this.state.showSideDrawer}
-          closed={this.sideDrawerClosedHandler}
-        />
-        <main className={classes.Content}>{this.props.children}</main>
-      </Aux>
-    );
-  }
+    render() {
+        return (
+            <Aux>
+                <Toolbar 
+                    isAuth={this.props.isAuthenticated}
+                    drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer
+                    isAuth={this.props.isAuthenticated}
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler}
+                />
+                <main className={classes.Content}>{this.props.children}</main>
+            </Aux>
+        );
+    }
 }
 
-export default Layout;
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.auth.token !== null
+    }
+}
+
+export default connect(mapStateToProps)(Layout);
